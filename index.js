7933b60e53fadb3c7e3b95a025c48888f9311e11
Lior <levydlior@gmail.com>
2022-04-07 03:35:39 -0600

    Add responsive burger menu

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
   }
-
-  
